0227b9b10002b1c033829eb276940e100ff6af71
bepyan <bepyan@naver.com>
2022-01-17 19:07:40 +0900

    style. 자판기 버튼 클릭 여부 스타일 추가

--- src/components/VendingItem.tsx
+++ src/components/VendingItem.tsx
@@ -1,6 +1,6 @@
 import { IProductStock } from '@types';
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 interface Props {
     onItemClickHandler: (product: IProductStock) => void;
@@ -10,6 +10,7 @@ interface Props {
     };
 }
 
+export const VendingItem = (props: Props) => {
 const selectProduct = (
     product: IProductStock,
     onItemClickHandler: (product: IProductStock) => void,
@@ -18,36 +19,47 @@ const selectProduct = (
     onItemClickHandler(product);
 };
 
-export const VendingItem = (props: Props) => {
-    const { onItemClickHandler, product } = props;
     return (
-        <Wrapper>
+        <Wrapper isSeletable={true} onClick={selectProduct}>
             <Title>{product.name}</Title>
             <Price>
                 <span>{product.price}</span>
                 <span className="txt__unit">원</span>
             </Price>
+            <div>{product.stock}</div>
         </Wrapper>
     );
 };
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<{ isSeletable: boolean }>`
     display: flex;
     flex-direction: column;
     background-color: var(--content-bg);
     border-radius: 14px;
     border: 1px solid var(--theme-bg-color);
     padding: 1rem;
-    cursor: pointer;
-    transition: transform 300ms ease-in-out;
+    user-select: none;
 
-    &:hover {
-        transform: scale(1.02);
-        background-color: var(--theme-bg-color);
-    }
+    ${({ isSeletable }) =>
+        isSeletable
+            ? css`
+                  cursor: pointer;
+                  transition: transform 300ms ease-in-out;
+                  &:hover {
+                      transform: scale(1.02);
+                      background-color: var(--theme-bg-color);
+                  }
+              `
+            : css`
+                  pointer-events: none;
+                  cursor: default;
+                  opacity: 0.5;
+              `}
 `;
 
-const Title = styled.div``;
+const Title = styled.div`
+    word-break: keep-all;
+`;
 
 const Price = styled.div`
     margin-top: auto;
